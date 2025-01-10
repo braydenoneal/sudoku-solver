08b9bd7b765838955fb3447ca55acb252e0ced9b
Brayden O'Neal <braydenoneal@protonmail.com>
2025-01-10 12:44:54 -0600

    Implement dfs backtracking

--- src/board_dfs.rs
+++ src/board_dfs.rs
@@ -9,30 +9,56 @@ pub struct Board {
 }
 
 impl Board {
-    fn is_valid(&self, row: usize, col: usize) -> bool {
-        for number in 0..9 {
-            if self.cells[row].contains(&(number as Cell)) {
-                return false;
-            }
+    fn check_number(&self, number: usize, row: usize, col: usize) -> bool {
+        if self.cells[row].contains(&(number as Cell)) {
+            return false;
+        }
 
-            if self
-                .cells
-                .iter()
-                .map(|row| row[col])
-                .any(|cell| cell == number as Cell)
-            {
-                return false;
-            }
+        if self
+            .cells
+            .iter()
+            .map(|row| row[col])
+            .any(|cell| cell == number as Cell)
+        {
+            return false;
+        }
 
-            let col = col / 3;
-            let row = row / 3;
+        let col = col / 3;
+        let row = row / 3;
 
-            for subgrid_row in 0..3 {
-                for subgrid_col in 0..3 {
-                    if self.cells[row * 3 + subgrid_row][col * 3 + subgrid_col] == number as u8 {
-                        return false;
-                    }
+        for subgrid_row in 0..3 {
+            for subgrid_col in 0..3 {
+                if self.cells[row * 3 + subgrid_row][col * 3 + subgrid_col] == number as u8 {
+                    return false;
                 }
+            }
+        }
+
+        true
+    }
+
+    pub fn solve(&mut self) -> bool {
+        for row in 0..9 {
+            for col in 0..9 {
+                if self.cells[row][col] != 0 {
+                    continue;
+                }
+
+                for number in 1..=9 {
+                    if !self.check_number(number, row, col) {
+                        continue;
+                    }
+
+                    self.cells[row][col] = number as Cell;
+
+                    if self.solve() {
+                        return true;
+                    }
+
+                    self.cells[row][col] = 0;
+                }
+
+                return false;
             }
         }
 
@@ -62,12 +88,6 @@ impl Board {
             }
 
             println!();
-        }
-    }
-
-    pub fn solve(&mut self) {
-        loop {
-            break;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,11 @@ fn main() {
     //
     // assert_eq!(board.cells(), example_board.solution);
 
-    read_csv("sudoku.csv").unwrap();
+    // read_csv("sudoku.csv").unwrap();
     // read_csv2("sudoku.csv").unwrap();
     // read_csv3("sudoku-3m.csv").unwrap();
+    read_csv4("sudoku.csv").unwrap();
+    // dfs_3_mil("sudoku-3m.csv").unwrap();
 }
 
 use std::error::Error;
@@ -183,6 +185,130 @@ fn read_csv3<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
         }
 
         let mut board = board2::Board::new(example_board.puzzle);
+
+        use std::time::Instant;
+        let now = Instant::now();
+
+        board.solve();
+
+        total_duration += now.elapsed();
+
+        let average_duration = total_duration / count;
+
+        let eq = std::panic::catch_unwind(|| assert_eq!(board.cells(), example_board.solution));
+
+        if eq.is_err() {
+            incorrect_counts.push(count);
+        }
+
+        println!("Count: {count}, Average: {average_duration:?} Incorrect: {incorrect_counts:?}");
+
+        count += 1;
+    }
+
+    Ok(())
+}
+
+fn read_csv4<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    let mut count = 1;
+    let mut incorrect_counts = Vec::new();
+    let mut total_duration = std::time::Duration::new(0, 0);
+
+    for result in rdr.records() {
+        let record = result?;
+
+        let mut example_board = board::ExampleBoards {
+            puzzle: [[0; 9]; 9],
+            solution: [[0; 9]; 9],
+        };
+
+        for i in 0..=1 {
+            let board_string = &record[i];
+
+            board_string
+                .chars()
+                .map(|c| c.to_string().parse::<u8>().unwrap())
+                .enumerate()
+                .for_each(|(index, value)| match i {
+                    0 => {
+                        example_board.puzzle[index / 9][index % 9] = value;
+                    }
+                    _ => {
+                        example_board.solution[index / 9][index % 9] = value;
+                    }
+                });
+        }
+
+        let mut board = board_dfs::Board::new(example_board.puzzle);
+
+        use std::time::Instant;
+        let now = Instant::now();
+
+        board.solve();
+
+        total_duration += now.elapsed();
+
+        let eq = std::panic::catch_unwind(|| assert_eq!(board.cells(), example_board.solution));
+
+        if eq.is_err() {
+            incorrect_counts.push(count);
+        }
+
+        let average_duration = total_duration / count;
+
+        println!("Count: {count}, Average: {average_duration:?} Incorrect: {incorrect_counts:?}");
+
+        count += 1;
+    }
+
+    Ok(())
+}
+
+fn dfs_3_mil<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    let mut count = 1;
+    let mut incorrect_counts: Vec<u32> = Vec::new();
+    let mut total_duration = std::time::Duration::new(0, 0);
+
+    for result in rdr.records() {
+        let record = result?;
+
+        // if &record[4] != "0.0" {
+        //     continue;
+        // }
+
+        let mut example_board = board::ExampleBoards {
+            puzzle: [[0; 9]; 9],
+            solution: [[0; 9]; 9],
+        };
+
+        for i in 0..=1 {
+            let board_string = &record[i + 1];
+
+            board_string
+                .chars()
+                .map(|c| match c {
+                    '.' => '0',
+                    _ => c,
+                })
+                .map(|c| c.to_string().parse::<u8>().unwrap())
+                .enumerate()
+                .for_each(|(index, value)| match i {
+                    0 => {
+                        example_board.puzzle[index / 9][index % 9] = value;
+                    }
+                    _ => {
+                        example_board.solution[index / 9][index % 9] = value;
+                    }
+                });
+        }
+
+        let mut board = board_dfs::Board::new(example_board.puzzle);
 
         use std::time::Instant;
         let now = Instant::now();
